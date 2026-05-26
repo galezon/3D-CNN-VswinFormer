e75d07239a3f13fc16592c31ecb8d979129b50b2
Lunkyadi Kurniawan Sucipto <lunkyadikurniawan.sucipto@p-vibcompute-1.vib.local>
2026-05-26 13:33:45 +0200

    moved some files in data storage, adjusted register accordingly

--- register.py
+++ register.py
@@ -13,16 +13,20 @@ train_ready_registered_datasets = [
 
 paths_cluster = {
     "base_metadata_path": "/data/timeleap-shared/MRIs/metadata",
-    "adni3_nifti_dir": "/data/timeleap-shared/MRIs/preprocessed/ADNI3",
+    "adni3_nifti_dir": "/data/timeleap-shared/MRIs/preprocessed/ADNI3",  # preprocessed but not smoothed
     "ixi_nifti_dir": "/data/timeleap-shared/MRIs/preprocessed_IXI-T1",
     "adni_1_2_go_nifti_dir": "/data/timeleap-shared/MRIs/preprocessed/ADNI_1_2_GO",
+    "adni3_smoothed_dir": "/data/timeleap-shared/MRIs/preprocessed/ADNI3",
+    "adni_1_2_go_smoothed_dir": "/data/timeleap-shared/MRIs/preprocessed/ADNI_1_2_GO"
 }
 
 paths_datacore = {
     "base_metadata_path": "/data/groups/cmn/valeriya.malysheva/lunky/metadata",
-    "adni3_nifti_dir": "/data/groups/cmn/valeriya.malysheva/lunky/ADNI3",
+    "adni3_nifti_dir": "/data/groups/cmn/valeriya.malysheva/lunky/MRIs/preprocessed/ADNI3_preprocessed",  # preprocessed but not smoothed
     "ixi_nifti_dir": "/data/groups/cmn/valeriya.malysheva/lunky/IXI",  # not in datacore just yet
     "adni_1_2_go_nifti_dir": "/data/groups/cmn/valeriya.malysheva/margo/ADNI_1_2_GO",
-    "ADNI3_ctf_3d": "/data/groups/cmn/valeriya.malysheva/lunky/ctf_3d/ADNI3/",
-    "ADNI_1_2_GO_ctf_3d": "/data/groups/cmn/valeriya.malysheva/lunky/ctf_3d/ADNI_1_2_GO/"
+    "ADNI3_ctf_3d": "/data/groups/cmn/valeriya.malysheva/lunky/MRIs/ctf_3d/ADNI3/",
+    "ADNI_1_2_GO_ctf_3d": "/data/groups/cmn/valeriya.malysheva/lunky/MRIs/ctf_3d/ADNI_1_2_GO/",
+    "adni3_smoothed_dir": "/data/groups/cmn/valeriya.malysheva/lunky/MRIs/smoothed/ADNI3",
+    "adni_1_2_go_smoothed_dir": "/data/groups/cmn/valeriya.malysheva/lunky/MRIs/smoothed/ADNI_1_2_GO"
 }
